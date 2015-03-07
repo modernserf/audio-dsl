d19b66e9a90b34007bf55262b8dcffd10161175e
Justin Falcone <stanulam59@gmail.com>
2015-03-07 11:32:19 -0500

    value streams, basic theremin

--- src/main.js
+++ src/main.js
@@ -20,12 +20,52 @@ class NodeWrapper {
     }
 }
 
+class ValueState {
+    constructor (params) {
+        this.value = params.value;
+        this.listeners = params.listeners;
+    }
+    onUpdate (fn) {
+        this.listeners.push(fn);
+    }
+    set (value) {
+        this.value = value;
+        this.publish();
+    }
+    publish () {
+        this.listeners.forEach((l) => {
+            l(this.value);
+        });
+    }
+}
+
+ValueState.create = (value) => {
+    const newState = new ValueState({
+        value: value,
+        listeners: []
+    });
+    newState.publish();
+    return newState;
+};
+
+const link = (src, dest) => {
+    if (src instanceof ValueState) {
+        dest.value = src.value;
+        src.onUpdate((v) => {
+            dest.value = v;
+        });
+    } else {
+        dest.value = src;
+    }
+};
+
 function Osc (params) {
     const node = audioCtx.createOscillator();
     node.type = params.type;
-    // TODO: handle streams
-    node.frequency.value = params.frequency;
-    node.detune.value = params.detune || 0;
+
+    link(params.frequency, node.frequency);
+    link(params.detune || 0, node.detune);
+
     node.start();
 
     return new NodeWrapper(node);
@@ -33,16 +73,17 @@ function Osc (params) {
 
 function VCF (params) {
     const node = audioCtx.createBiquadFilter();
-    node.frequency.value = params.cutoff;
-    node.Q.value = params.resonance;
+
     node.type = params.type;
+    link(params.cutoff, node.frequency);
+    link(params.resonance, node.Q);
 
     return new NodeWrapper(node);
 }
 
 function VCA (params) {
     const node = audioCtx.createGain();
-    node.gain.value  = params.gain;
+    link(params.gain, node.gain);
 
     return new NodeWrapper(node);
 }
@@ -54,7 +95,7 @@ function Mixer (params) {
     channels.forEach((ch)=> {
         const [audio, level] = ch;
         const gain = audioCtx.createGain();
-        gain.gain.value = level;
+        link(level, gain.gain);
 
         audio.rawNode.connect(gain);
         gain.connect(out);
@@ -63,16 +104,30 @@ function Mixer (params) {
     return new NodeWrapper(out);
 }
 
-getOutput(()=> {
-    const osc1 = Osc({type: "sawtooth",frequency: 220});
-    const osc2 = Osc({type: "sawtooth",frequency: 220, detune: 5});
+document.addEventListener('DOMContentLoaded', ()=> {
+    let pad = document.getElementById('xy-pad');
 
-    Mixer({channels: [[osc1, 1],[osc2, 1]]})
-        .connect(VCF({type: "lowpass", cutoff: 2000, resonance: 0.5}))
-        .connect(VCA({gain: 0.5}));
+    let freq = ValueState.create(220);
+    let gain = ValueState.create(0);
+
+    getOutput(()=> {
+        const osc1 = Osc({type: "sawtooth",frequency: freq});
+        const osc2 = Osc({type: "sawtooth",frequency: freq, detune: 20});
+
+        return Mixer({channels: [[osc1, 1],[osc2, 1]]})
+            .connect(VCF({type: "lowpass", cutoff: 2000, resonance: 0.5}))
+            .connect(VCA({gain: gain}));
+    });
+
+    pad.addEventListener('scroll', (e)=> {
+        const { scrollTop, scrollLeft } = e.target;
+
+        console.log(scrollTop,scrollLeft);
+
+        freq.set(220 +  220 * scrollTop/300);
+        gain.set(scrollLeft/300);
+    });
 });
-
-
 
 
 
